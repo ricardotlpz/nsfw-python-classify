2a1d50afaff99ab19b6bb2961622309c91d87640
ricardotlpz <ricardo-tejada@live.com.mx>
2022-03-04 17:00:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import imageio
 
 from nsfw import classify
 
-image = imageio.imread("images/image.jpg")
+image = imageio.imread("images/image.jpg", pilmode="RGB")
 sfw, nsfw = classify(image)
 
 print("SFW Probability: {}".format(sfw))
