3f729d3f21f0a38c2e57a9c88df40cd4ce1ea60e
ricardotlpz <ricardo-tejada@live.com.mx>
2022-03-04 16:24:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import PIL.Image as Image
 
 from nsfw import classify
 
-image = Image.open("/images/image.jpg")
+image = Image.open("images/image.jpg")
 sfw, nsfw = classify(image)
 
 print("SFW Probability: {}".format(sfw))
